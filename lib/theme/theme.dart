0023f2bcf1c39919ab7ea057ae47c4c34f9edffc
KwonCheol <pes2012a@gmail.com>
2020-12-01 16:05:38 +0900

    모양 완성. theme만들고 버튼들 동작하도록 구현 해야 함.

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class palette {
   static Color highlight1 = Color(0xff6977E4);
+}
+
+class OKButton extends StatefulWidget {
+  final Widget _child;
+  final Function _onPressed;
+  OKButton({@required Widget child, @required Function onPressed})
+      : _child = child,
+        _onPressed = onPressed;
+  @override
+  State<StatefulWidget> createState() => _OKButtonState(child: _child, onPressed: _onPressed);
+}
+
+class _OKButtonState extends State<OKButton> {
+  final Widget _child;
+  final Function _onPressed;
+  _OKButtonState({@required Widget child, @required Function onPressed})
+      : _child = child,
+        _onPressed = onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: MediaQuery.of(context).size.width * (316 / 360),
+        height: MediaQuery.of(context).size.height * (40 / 640),
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(23)))),
+            onPressed: _onPressed,
+            child: _child));
+  }
+}
+
+class InstructionBalloon extends StatelessWidget {
+  String _text;
+  InstructionBalloon(text) : _text = text;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Center(child: SvgPicture.asset('images/svg/balloon.svg')),
+        Container(
+          margin: EdgeInsets.only(bottom: 10),
+          child: Text(
+            _text,
+            style: TextStyle(fontSize: 13, fontFamily: 'NanumSquare'),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
 final coreTheme = ThemeData(
