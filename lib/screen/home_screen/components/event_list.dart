27a68277854cffc019dd6deb3a8a9ff3fe081d85
SW <aliwo@naver.com>
2021-01-06 02:12:13 +0900

    Event List 를 형성하는 비즈니스 로직...
    함수로 빼든가 해야지

--- lib/screen/home_screen/components/event_list.dart
+++ lib/screen/home_screen/components/event_list.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 
 import 'package:acleane/bloc_layer/bloc/calendar/calendar_bloc.dart';
 import 'package:acleane/bloc_layer/bloc/journal/journal_bloc.dart';
+import 'package:acleane/bloc_layer/bloc/routine/routine_bloc.dart';
 import 'package:acleane/bloc_layer/model/journal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,16 +14,30 @@ class EventList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var journalState = BlocProvider.of<JournalBloc>(context).state;
     return BlocBuilder(
       bloc: BlocProvider.of<CalendarBloc>(context),
       builder: (context, state) {
-        if (state is CalendarSuccess) {
-          return ListView.builder(
-            itemCount: journalList[state.date]?.length ?? 0,
-            itemBuilder: (context, index) {
-              print(BlocProvider.of<JournalBloc>(context).state);
-              return _EventElement(journal: journalList[state.date][index]);
-            },
+        if (state is CalendarSuccess && journalState is JournalSuccess) {
+
+          // 먼저 각 userRoutine 별로 EventElement 를 형성
+          var events = {};
+          for (var userRoutine in journalState.userRoutines) {
+            events[userRoutine.routineId] = {'title': userRoutine.routineName, 'value': false};
+          }
+
+          // journals 를 회전하면서 routine_id 가 같은 userRoutine 이 있다면 value 를 true 로 set
+          var lefted = [];
+          for (var journal in journalList[state.date] ?? []) {
+            if (events.containsKey(journal.routineId)) {
+              events[journal.routineId]['value'] = true;
+            } else {
+              events[journal.routineId] = {'title': journal.routineName, 'value': true};
+            }
+          }
+
+          return ListView(
+            children: events.values.map((x) => _EventElement(title: x['title'], value: x['value'])).toList(),
           );
         }
         return Container();
@@ -30,13 +45,12 @@ class EventList extends StatelessWidget {
     );
   }
 
-  Widget _EventElement({Journal journal}) {
+  Widget _EventElement({String title, bool value}) {
     return Container(
       child: CheckboxListTile(
-        title: Text(journal.routineName),
-        secondary: Text('종류'),
+        title: Text(title),
         controlAffinity: ListTileControlAffinity.trailing,
-        value: true,
+        value: value,
         onChanged: (bool value) {
           // journal 을 베이스로 만들어진 CheckboxListTile 은 무조건 true
           print('checkbox Changed');
