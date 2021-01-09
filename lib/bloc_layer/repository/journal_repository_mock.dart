8275755c9bba6b36bd17d4383510e146c6311cf9
SW <aliwo@naver.com>
2021-01-09 12:34:16 +0900

    Mock ID 통일

--- lib/bloc_layer/repository/journal_repository_mock.dart
+++ lib/bloc_layer/repository/journal_repository_mock.dart
@@ -14,7 +14,7 @@ class JournalRepositoryMock extends JournalRepository {
     return {
       _selectedDay.subtract(Duration(days: 30)): [
         Journal(id: 1, userId: 1, routineId: 1, routineName: '로아큐탄', date: _selectedDay.subtract(Duration(days: 30))),
-        Journal(id: 1, userId: 1, routineId: 2, routineName: '비타민D', date: _selectedDay.subtract(Duration(days: 30))),
+        Journal(id: 1, userId: 1, routineId: 10, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 30))),
         Journal(id: 1, userId: 1, routineId: 3, routineName: '미노사이클린', date: _selectedDay.subtract(Duration(days: 30))),
       ],
       _selectedDay.subtract(Duration(days: 10)): [
@@ -22,27 +22,27 @@ class JournalRepositoryMock extends JournalRepository {
       ],
       _selectedDay.subtract(Duration(days: 3)): [
         Journal(id: 1, userId: 1, routineId: 1, routineName: '로아큐탄', date: _selectedDay.subtract(Duration(days: 20))),
-        Journal(id: 1, userId: 1, routineId: 2, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 20))),
+        Journal(id: 1, userId: 1, routineId: 10, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 20))),
         Journal(id: 1, userId: 1, routineId: 3, routineName: '미노사이클린', date: _selectedDay.subtract(Duration(days: 20))),
-        Journal(id: 1, userId: 1, routineId: 4, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 20))),
+        Journal(id: 1, userId: 1, routineId: 8, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 20))),
       ],
       _selectedDay.subtract(Duration(days: 2)): [
         Journal(id: 1, userId: 1, routineId: 1, routineName: '로아큐탄', date: _selectedDay.subtract(Duration(days: 16))),
-        Journal(id: 1, userId: 1, routineId: 2, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 16))),
+        Journal(id: 1, userId: 1, routineId: 10, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 16))),
       ],
       _selectedDay.add(Duration(days: 10)): [
         Journal(id: 1, userId: 1, routineId: 3, routineName: '미노사이클린', date: _selectedDay.subtract(Duration(days: 10))),
-        Journal(id: 1, userId: 1, routineId: 4, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 10))),
-        Journal(id: 1, userId: 1, routineId: 2, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 10))),
+        Journal(id: 1, userId: 1, routineId: 8, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 10))),
+        Journal(id: 1, userId: 1, routineId: 10, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 10))),
       ],
       _selectedDay.add(Duration(days: 4)): [
         Journal(id: 1, userId: 1, routineId: 3, routineName: '미노사이클린', date: _selectedDay.subtract(Duration(days: 4))),
-        Journal(id: 1, userId: 1, routineId: 4, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 4))),
-        Journal(id: 1, userId: 1, routineId: 2, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 4))),
+        Journal(id: 1, userId: 1, routineId: 8, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 4))),
+        Journal(id: 1, userId: 1, routineId: 10, routineName: '비타민 D', date: _selectedDay.subtract(Duration(days: 4))),
       ],
       _selectedDay.add(Duration(days: 2)): [
         Journal(id: 1, userId: 1, routineId: 3, routineName: '미노사이클린', date: _selectedDay.subtract(Duration(days: 2))),
-        Journal(id: 1, userId: 1, routineId: 4, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 2))),
+        Journal(id: 1, userId: 1, routineId: 8, routineName: '디페린', date: _selectedDay.subtract(Duration(days: 2))),
       ]
     };
   }
--- lib/bloc_layer/repository/routine_repository_mock.dart
+++ lib/bloc_layer/repository/routine_repository_mock.dart
@@ -11,8 +11,17 @@ class RoutineRepositoryMock extends RoutineRepository {
   @override
   Future<dynamic> getAllRoutines() async {
     return [
+      // xd 기준
       Routine(id:1, name: '로아큐탄', amounts: ['1알', '2알', '3알', '4알']),
-      Routine(id:2, name: '미노사이클린', amounts: ['1알', '2알', '3알', '4알']),
+      Routine(id:2, name: '독시사이클린', amounts: ['1알', '2알', '3알', '4알']),
+      Routine(id:3, name: '미노사이클린', amounts: ['1알', '2알', '3알', '4알']),
+      Routine(id:4, name: 'AHA', amounts: ['적정량']),
+      Routine(id:5, name: 'BHA', amounts: ['적정량']),
+      Routine(id:6, name: 'PHA', amounts: ['적정량']),
+      Routine(id:7, name: '벤조일페록사이드', amounts: ['한 방울', '두 방울', '세 방울', '10원 동전 크기']),
+      Routine(id:8, name: '디페린', amounts: ['한 방울', '두 방울', '세 방울', '10원 동전 크기']),
+      Routine(id:9, name: '비타민 A', amounts: ['3000IU', '10000IU', '20000IU', '30000IU']),
+      Routine(id:10, name: '비타민 D', amounts: ['3000IU', '10000IU', '20000IU', '30000IU']),
     ];
   }
 
@@ -20,7 +29,7 @@ class RoutineRepositoryMock extends RoutineRepository {
   Future<dynamic> getAllUserRoutines() async {
     return [
       UserRoutine(id: 1, routineId: 1, routineName: '로아큐탄', amount: '1 알'),
-      UserRoutine(id: 2, routineId: 2, routineName: '미노사이클린', amount: '2 알')
+      UserRoutine(id: 2, routineId: 2, routineName: '독시사이클린', amount: '2 알')
     ];
   }
 }
