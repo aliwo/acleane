76d6405e4b538fa904884a7dc33eb6ea6a625e83
seoljaehyeok <milkboy2564@naver.com>
2021-03-14 16:45:30 +0900

    Home Screen에서 버튼 누르면 SelectRoutine 스크린으로 라우팅

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -3,15 +3,16 @@ import 'package:acleane/bloc_layer/bloc/calendar/calendar_bloc.dart';
 import 'package:acleane/bloc_layer/bloc/journal/journal_bloc.dart';
 import 'package:acleane/screen/home_screen/components/custom_calendar.dart';
 import 'package:acleane/screen/home_screen/components/event_list.dart';
+import 'package:acleane/screen/select_routine.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:acleane/theme/theme.dart';
 
 class HomeScreen extends StatelessWidget {
   final String title = '곰보일기';
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -29,8 +30,18 @@ class HomeScreen extends StatelessWidget {
                     ? EventList(journalList: state.journals)
                     : CircularProgressIndicator(),
               ),
-              FloatingActionButton(
-                onPressed: () {},
+              Card(
+                child: IconButton(
+                  icon: Icon(Icons.create),
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => SelectRoutine()));
+                  },
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                color: coreTheme.primaryColor,
               ),
             ],
           );
---
+++ lib/screen/select_routine.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectRoutine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('루틴 입력'),
+      ),
+      body: Center(
+        child: Text('루틴 입력 페이지'),
+      ),
+    );
+  }
+}
